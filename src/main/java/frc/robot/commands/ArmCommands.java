30d18f4ebb356a5598a862df20fb6bd52180f105
Ryan Yeung <ryanyeung@frc4322.com>
2025-06-27 14:56:22 -0700

    Still kinda gets stuck

--- src/main/java/frc/robot/commands/ArmCommands.java
+++ src/main/java/frc/robot/commands/ArmCommands.java
@@ -4,11 +4,14 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.arm.Arm;
+import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
 
 public class ArmCommands extends Command {
-  private double kP = 0.6;
-  private double kI = 0;
-  private double kD = 0;
+  private LoggedNetworkNumber requestedPositionDeg =
+      new LoggedNetworkNumber("Arm/RequestedPositionDeg", 0.0);
+  private LoggedNetworkNumber kP = new LoggedNetworkNumber("Arm/kP", 0.7);
+  private LoggedNetworkNumber kI = new LoggedNetworkNumber("Arm/kI", 0);
+  private LoggedNetworkNumber kD = new LoggedNetworkNumber("Arm/kD", 0.2);
 
   private double setPoint = 0;
   private double currentPoint;
@@ -20,10 +23,10 @@ public class ArmCommands extends Command {
 
   private boolean isInited = false;
 
-  double[] queuedPositions = {45, 135, 0, -179, 179, -90, 90};
+  double[] queuedPositions = {45, 135, 0, -180, 180, -90, 90};
   int timesRun = 0;
 
-  PIDController pid = new PIDController(kP, kI, kD);
+  PIDController pid = new PIDController(0, 0, 0);
 
   private Arm arm;
 
@@ -33,30 +36,36 @@ public class ArmCommands extends Command {
     System.out.println("Initialized instance " + ownInstance + " of ArmCommands");
     this.arm = arm;
     addRequirements(arm);
+    pid.enableContinuousInput(-180.0, 180.0);
   }
 
   @Override
   public void initialize() {
+    if (intializedTimes > 6) {
+      intializedTimes = 0;
+    }
     isInited = true;
     System.out.println("Initialized " + intializedTimes + " times");
-    intializedTimes++;
     System.out.println("Set point to " + queuedPositions[timesRun]);
     setPoint = queuedPositions[timesRun];
     timesRun++;
+    intializedTimes++;
   }
 
   @Override
   public void execute() {
+    pid.setPID(kP.get(), kI.get(), kD.get());
     currentPoint = arm.getPositionDeg();
     output = pid.calculate(currentPoint, setPoint);
     arm.setVoltage(output);
-    System.out.println("Current: " + arm.getPositionDeg() + " | Target: " + setPoint);
-    // System.out.println(output);
+    System.out.println("Current: " + currentPoint + " | Target: " + setPoint);
+    System.out.println("kP: " + kP.get() + " | kI: " + kI.get() + " | kD: " + kD.get());
+    System.out.println(output);
   }
 
   @Override
   public boolean isFinished() {
-    return MathUtil.isNear(setPoint, currentPoint, 1);
+    return MathUtil.isNear(setPoint, currentPoint, 5);
   }
 
   @Override
