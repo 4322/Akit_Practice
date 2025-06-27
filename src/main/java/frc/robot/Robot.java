c8ce4e8c031f37cf993ebf14c8c28ed7c8172001
Ellie Morse <elliemorse@frc4322.com>
2025-06-27 12:54:14 -0700

    Deleted accidental instance

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,9 +13,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Threads;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
@@ -23,6 +20,10 @@ import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 import org.littletonrobotics.urcl.URCL;
+
+import edu.wpi.first.wpilibj.Threads;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -152,7 +153,7 @@ public class Robot extends LoggedRobot {
       autonomousCommand.cancel();
     }
 
-    m_RobotContainer.elevator1.teleopInit();
+  
   }
 
   /** This function is called periodically during operator control. */
