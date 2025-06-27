55506cbc189edb41b8d67d32fe3d1376d99f7e71
Ellie Morse <elliemorse@frc4322.com>
2025-06-27 12:52:17 -0700

    Moved something pritter

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -1,8 +1,9 @@
 package frc.robot.subsystems.elevator;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import org.littletonrobotics.junction.Logger;
 
 public class Elevator extends SubsystemBase {
   private ElevatorIO io;
@@ -30,7 +31,7 @@ public class Elevator extends SubsystemBase {
   }
 
   public void teleopInit() {
-    timer.restart();
+   timer.restart();
   }
 
   @Override
@@ -74,4 +75,5 @@ public class Elevator extends SubsystemBase {
         break;
     }
   }
+
 }
