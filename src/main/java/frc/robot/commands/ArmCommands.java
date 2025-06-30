3485c57a6c6fd81ef71e2e393256b91af616e477
Ellie Morse <elliemorse@frc4322.com>
2025-06-30 01:36:56 -0700

    Won't work

--- src/main/java/frc/robot/commands/ArmCommands.java
+++ src/main/java/frc/robot/commands/ArmCommands.java
@@ -1,13 +1,12 @@
 package frc.robot.commands;
 
-import org.littletonrobotics.junction.Logger;
-import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
-
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.arm.Arm;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
 
 public class ArmCommands extends Command {
 
@@ -57,177 +56,174 @@ public class ArmCommands extends Command {
     Logger.recordOutput("Arm/currentPosition", currentPosition);
     Logger.recordOutput("Arm/requestedPositionBeforeOptimize", requestedPosition);
 
-    // Optimize requested position to minimize rotation
-    double UwU = Math.abs(requestedPosition - currentPosition);
-    if (currentPosition > 0) {
-      if (currentPosition < 0) {
-        if (requestedPosition > 0) {
-          if (UwU > 180) {
-            requestedPosition -= 360;
-          }
-        }
-      } else if (currentPosition > 0) {
-        if (requestedPosition < 0) {
-          if (UwU > 180) {
-            requestedPosition += 360;
-          }
+    if (currentPosition < 0) {
+      if (requestedPosition > 0) {
+        if (Math.abs(requestedPosition - currentPosition) > 180) {
+          requestedPosition -= 360;
         }
       }
-
-      if (requestedPosition > 270) {
-        requestedPosition -= 360;
-      } else if (requestedPosition < -270) {
-        requestedPosition += 360;
+    } else if (currentPosition > 0) {
+      if (requestedPosition < 0) {
+        if (Math.abs(requestedPosition - currentPosition) > 180) {
+          requestedPosition += 360;
+        }
       }
-
-      switch (armState) {
-        case DEG_NONE:
-          requestedPositionDeg.set(0.0);
-          if (currentPosition >= -0.1 || currentPosition <= 0.1) {
-            timer.start();
-            if (timer.hasElapsed(0.5)) {
-              armState = ArmState.DEG_45;
-              timer.stop();
-              timer.reset();
-            }
-          } else {
-            timer.stop();
-            timer.reset();
-          }
-          break;
-        case DEG_45:
-          requestedPositionDeg.set(45.0);
-          if (currentPosition <= 45.1 || currentPosition >= 44.9) {
-            timer.start();
-            if (timer.hasElapsed(1.5)) {
-              armState = ArmState.DEG_179;
-              timer.stop();
-              timer.reset();
-            }
-          } else {
-            timer.stop();
-            timer.reset();
-          }
-          break;
-        case DEG_135:
-          requestedPositionDeg.set(135.0);
-          if (currentPosition <= 135.1 || currentPosition >= 134.9) {
-            timer.start();
-            if (timer.hasElapsed(1.5)) {
-              armState = ArmState.DEG_0;
-              timer.stop();
-              timer.reset();
-            }
-          } else {
-            timer.stop();
-            timer.reset();
-          }
-          break;
-        case DEG_0:
-          requestedPositionDeg.set(0.0);
-          if (requestedPosition <= 0.1 || currentPosition >= -0.1 || currentPosition >= -0.1) {
-            timer.start();
-            if (timer.hasElapsed(1.5)) {
-              armState = ArmState.DEG_NEG_179;
-              timer.stop();
-              timer.reset();
-            }
-          } else {
-            timer.stop();
-            timer.reset();
-          }
-          break;
-        case DEG_NEG_179:
-          requestedPositionDeg.set(-179.0);
-          if (currentPosition <= -179.1 || currentPosition >= -178.9) {
-            timer.start();
-            if (timer.hasElapsed(1.5)) {
-              armState = ArmState.DEG_NEG_90;
-              timer.stop();
-              timer.reset();
-            }
-          } else {
-            timer.stop();
-            timer.reset();
-          }
-          break;
-        case DEG_179:
-          requestedPositionDeg.set(179.0);
-          if (currentPosition <= 179.1 || currentPosition >= 178.9) {
-            timer.start();
-            if (timer.hasElapsed(1.5)) {
-              armState = ArmState.DEG_NEG_179;
-              timer.stop();
-              timer.reset();
-            }
-          } else {
-            timer.stop();
-            timer.reset();
-          }
-          break;
-        case DEG_NEG_90:
-          requestedPositionDeg.set(-90.0);
-          if (currentPosition >= -90.1 || currentPosition <= -89.9) {
-            timer.start();
-            if (timer.hasElapsed(1.5)) {
-              armState = ArmState.DEG_NEG_45;
-              timer.stop();
-              timer.reset();
-            }
-          } else {
-            timer.stop();
-            timer.reset();
-          }
-          break;
-        case DEG_90:
-          requestedPositionDeg.set(90.0);
-          if (currentPosition <= 90.1 || currentPosition >= 89.9) {}
-          break;
-        case DEG_NEG_45:
-          requestedPositionDeg.set(-45.0);
-          if (currentPosition <= -45.1 || currentPosition >= -44.9) {
-            timer.start();
-            if (timer.hasElapsed(1.5)) {
-              armState = ArmState.DEG_180;
-              timer.stop();
-              timer.reset();
-            }
-          } else {
-            timer.stop();
-            timer.reset();
-          }
-          break;
-        case DEG_180:
-          requestedPositionDeg.set(180.0);
-          if (currentPosition <= -45.1 || currentPosition >= -44.9) {
-            timer.start();
-            if (timer.hasElapsed(1.5)) {
-              armState = ArmState.DEG_85;
-              timer.stop();
-              timer.reset();
-            }
-          } else {
-            timer.stop();
-            timer.reset();
-          }
-          break;
-        case DEG_85:
-          requestedPositionDeg.set(85.0);
-          if (currentPosition <= 85.1 || currentPosition >= 84.9) {
-            timer.start();
-            if (timer.hasElapsed(1.5)) {
-              timer.stop();
-              timer.reset();
-            }
-          } else {
-            timer.stop();
-            timer.reset();
-          }
-          break;
-      }
-      Logger.recordOutput("Arm/requestedPosition", requestedPosition);
-      Logger.recordOutput("Arm/calculatedVolts", output);
     }
+
+    if (requestedPosition > 270) {
+      requestedPosition -= 360;
+    } else if (requestedPosition < -270) {
+      requestedPosition += 360;
+    }
+
+    switch (armState) {
+      case DEG_NONE:
+        armState = ArmState.DEG_45;
+        requestedPositionDeg.set(0.0);
+        if (currentPosition >= -0.1 || currentPosition <= 0.1) {
+          timer.start();
+          if (timer.hasElapsed(0.5)) {
+            armState = ArmState.DEG_45;
+            timer.stop();
+            timer.reset();
+          }
+        } else {
+          timer.stop();
+          timer.reset();
+        }
+        break;
+      case DEG_45:
+        requestedPositionDeg.set(45.0);
+        if (currentPosition <= 45.1 || currentPosition >= 44.9) {
+          timer.start();
+          if (timer.hasElapsed(1.5)) {
+            armState = ArmState.DEG_179;
+            timer.stop();
+            timer.reset();
+          }
+        } else {
+          timer.stop();
+          timer.reset();
+        }
+        break;
+      case DEG_135:
+        requestedPositionDeg.set(135.0);
+        if (currentPosition <= 135.1 || currentPosition >= 134.9) {
+          timer.start();
+          if (timer.hasElapsed(1.5)) {
+            armState = ArmState.DEG_0;
+            timer.stop();
+            timer.reset();
+          }
+        } else {
+          timer.stop();
+          timer.reset();
+        }
+        break;
+      case DEG_0:
+        requestedPositionDeg.set(0.0);
+        if (requestedPosition <= 0.1 || currentPosition >= -0.1 || currentPosition >= -0.1) {
+          timer.start();
+          if (timer.hasElapsed(1.5)) {
+            armState = ArmState.DEG_NEG_179;
+            timer.stop();
+            timer.reset();
+          }
+        } else {
+          timer.stop();
+          timer.reset();
+        }
+        break;
+      case DEG_NEG_179:
+        requestedPositionDeg.set(-179.0);
+        if (currentPosition <= -179.1 || currentPosition >= -178.9) {
+          timer.start();
+          if (timer.hasElapsed(1.5)) {
+            armState = ArmState.DEG_NEG_90;
+            timer.stop();
+            timer.reset();
+          }
+        } else {
+          timer.stop();
+          timer.reset();
+        }
+        break;
+      case DEG_179:
+        requestedPositionDeg.set(179.0);
+        if (currentPosition <= 179.1 || currentPosition >= 178.9) {
+          timer.start();
+          if (timer.hasElapsed(1.5)) {
+            armState = ArmState.DEG_NEG_179;
+            timer.stop();
+            timer.reset();
+          }
+        } else {
+          timer.stop();
+          timer.reset();
+        }
+        break;
+      case DEG_NEG_90:
+        requestedPositionDeg.set(-90.0);
+        if (currentPosition >= -90.1 || currentPosition <= -89.9) {
+          timer.start();
+          if (timer.hasElapsed(1.5)) {
+            armState = ArmState.DEG_NEG_45;
+            timer.stop();
+            timer.reset();
+          }
+        } else {
+          timer.stop();
+          timer.reset();
+        }
+        break;
+      case DEG_90:
+        requestedPositionDeg.set(90.0);
+        if (currentPosition <= 90.1 || currentPosition >= 89.9) {}
+        break;
+      case DEG_NEG_45:
+        requestedPositionDeg.set(-45.0);
+        if (currentPosition <= -45.1 || currentPosition >= -44.9) {
+          timer.start();
+          if (timer.hasElapsed(1.5)) {
+            armState = ArmState.DEG_180;
+            timer.stop();
+            timer.reset();
+          }
+        } else {
+          timer.stop();
+          timer.reset();
+        }
+        break;
+      case DEG_180:
+        requestedPositionDeg.set(180.0);
+        if (currentPosition <= -45.1 || currentPosition >= -44.9) {
+          timer.start();
+          if (timer.hasElapsed(1.5)) {
+            armState = ArmState.DEG_85;
+            timer.stop();
+            timer.reset();
+          }
+        } else {
+          timer.stop();
+          timer.reset();
+        }
+        break;
+      case DEG_85:
+        requestedPositionDeg.set(85.0);
+        if (currentPosition <= 85.1 || currentPosition >= 84.9) {
+          timer.start();
+          if (timer.hasElapsed(1.5)) {
+            timer.stop();
+            timer.reset();
+          }
+        } else {
+          timer.stop();
+          timer.reset();
+        }
+        break;
+    }
+    Logger.recordOutput("Arm/requestedPosition", requestedPosition);
+    Logger.recordOutput("Arm/calculatedVolts", output);
   }
 
   @Override
