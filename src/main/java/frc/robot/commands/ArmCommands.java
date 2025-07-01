32723a153337e402dd87f6e6e8033d36fea8ac5d
Ryan Yeung <ryanyeung@frc4322.com>
2025-07-01 08:21:24 -0700

    Deleted command queueing logic

--- src/main/java/frc/robot/commands/ArmCommands.java
+++ src/main/java/frc/robot/commands/ArmCommands.java
@@ -18,12 +18,9 @@ public class ArmCommands extends Command {
 
   private static int instance;
   private int ownInstance;
-  private int intializedTimes = 0;
 
   private boolean isInited = false;
 
-  double[] queuedPositions = {45, 135, 0, -180, 180, -90, 90};
-  int timesRun = 0;
 
   PIDController pid = new PIDController(0, 0, 0);
   private Arm arm;
@@ -40,15 +37,7 @@ public class ArmCommands extends Command {
 
   @Override
   public void initialize() {
-    if (intializedTimes > 6) {
-      intializedTimes = 0;
-    }
     isInited = true;
-    System.out.println("Initialized " + intializedTimes + " times");
-    System.out.println("Set point to " + queuedPositions[timesRun]);
-    setPoint = queuedPositions[timesRun];
-    timesRun++;
-    intializedTimes++;
   }
 
   @Override
