543e97a7a68c31d5ebc8bc4876da1998f67cced8
Isaac Gallardo <isaacgallardo@frc4322.com>
2025-07-24 21:40:10 -0700

    e

--- src/main/java/frc/robot/subsystems/roller/Roller.java
+++ src/main/java/frc/robot/subsystems/roller/Roller.java
@@ -16,6 +16,7 @@ package frc.robot.subsystems.roller;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.function.DoubleSupplier;
+import org.littletonrobotics.junction.Logger;
 
 public class Roller extends SubsystemBase {
   private final RollerIO io;
