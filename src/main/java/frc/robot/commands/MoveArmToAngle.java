02da743d354fa30293dc1cebf1664b0d9da08350
Michelle <michelleli@frc4322.com>
2026-08-15 17:12:10 -0700

    fixed, testing commit

--- src/main/java/frc/robot/commands/MoveArmToAngle.java
+++ src/main/java/frc/robot/commands/MoveArmToAngle.java
@@ -1,44 +1,27 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.arm.Arm;
-import org.littletonrobotics.junction.Logger;
 
 public class MoveArmToAngle extends Command {
   private final Arm arm;
   private final double targetAngleDegrees;
 
-  private final PIDController pid = new PIDController(0.5, 0.0, 0.0);
 
   public MoveArmToAngle(Arm arm, double targetAngleDegrees) {
     this.arm = arm;
     this.targetAngleDegrees = targetAngleDegrees;
-
-    pid.setTolerance(1.0);
 
     addRequirements(arm);
   }
 
   @Override
   public void initialize() {
-    pid.reset();
-    pid.setSetpoint(targetAngleDegrees);
-  }
-
-  @Override
-  public void execute() {
-    double outputVoltage = pid.calculate(arm.getPositionDeg());
-    arm.setVoltage(outputVoltage);
-
-    Logger.recordOutput("Arm/TargetAngleDegree", targetAngleDegrees);
+    arm.setTargetAngle(targetAngleDegrees);
   }
 
   @Override
   public boolean isFinished() {
-    return pid.atSetpoint();
+    return arm.isAtSetPoint();
   }
-
-  @Override
-  public void end(boolean interrupted) {}
 }
--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.arm;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
 
@@ -8,18 +9,34 @@ public class Arm extends SubsystemBase {
 
   private ArmIO io;
   private ArmIOInputsAutoLogged inputs = new ArmIOInputsAutoLogged();
-
   private int instanceNum;
+
+  private final PIDController pid = new PIDController(1.0, 0.0, 0.0);
 
   public Arm(ArmIO io, int instanceNum) {
     this.io = io;
     this.instanceNum = instanceNum;
+
+    pid.setTolerance(1.0);
   }
 
   @Override
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("Arm " + instanceNum, inputs);
+
+    double outputVoltage = pid.calculate(getPositionDeg());
+    setVoltage(outputVoltage);
+  }
+
+  public void setTargetAngle(double targetAngleDegrees) {
+    pid.reset();
+    pid.setSetpoint(targetAngleDegrees);
+    Logger.recordOutput("Arms/TargetAngleDegree", targetAngleDegrees);
+  }
+
+  public boolean isAtSetPoint() {
+    return pid.atSetpoint();
   }
 
   public void setVoltage(double voltage) {
